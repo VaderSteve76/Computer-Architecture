cc3fe2775b5f2c3a39446e47a93a503d4f363c6c
VaderSteve76 <alverson.s@yahoo.com>
2019-07-24 16:18:08 -0400

    CPU init

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,26 +2,79 @@
 
 import sys
 
+# op codes
+HLT = 0b00000001
+LDI = 0b10000010
+PRN = 0b01000111
+MUL = 0b10100010
+PUSH = 0b01000101
+POP = 0b01000110
+
+# reserved reg.
+IM = 5
+IS = 6
+SP = 7
+
+# flags
+FL_LT = 0b100
+FL_GT = 0b010
+FL_EQ = 0b001
+FL_TIMER = 0b00000001
+FL_KEYBOARD = 0b00000010
+
 
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
         """Construct a new CPU."""
-        self.pc = 0
         self.ram = [0] * 256
         self.reg = [0] * 8
+        self.reg[SP] = 0xf4
+        self.processCounter = 0
+        self.flags = 0
+        self.isPaused = False
+        self.instruction_sets_processCounter = False
+        # self.interrupts = 1
+        # self.last_timer_int = None
 
-    def ram_read(self, MAR):
-        return self.ram[MAR]
+        self.branchTree = {
+            HLT: self.op_HLT,
+            LDI: self.op_LDI,
+            PRN: self.op_PRN,
+            MUL: self.op_MUL,
+            PUSH: self.op_PUSH,
+            POP: self.op_POP
+        }
 
-    def ram_write(self, MDR, MAR):
-        self.ram[MAR] = MDR
+    def ram_read(self, index):
+        return self.ram[index]
+
+    def ram_write(self, index, value):
+        self.ram[index] = value
+
+    def stack_push(self, val):
+        self.reg[SP] -= 1
+        self.ram_write(self.reg[SP], val)
+
+    def stack_pop(self):
+        val = self.ram_read(self.reg[SP])
+        self.reg[SP] += 1
+        return val
 
     def load(self):
         """Load a program into memory."""
-
         address = 0
+
+        fp = open(filename, "r")
+        for line in fp:
+            # split by comment and strip empty spaces
+            instruction = line.split("#")[0].strip()
+            if instruction == "":
+                continue
+            value = int(instruction, 2)
+            self.ram[address] = value
+            address += 1
 
         # For now, we've just hardcoded a program:
 
